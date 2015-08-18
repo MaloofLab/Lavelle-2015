70c87e15f0f93bb89dc65cbdd28d1d59018b2f5d
Julin Maloof <jnmaloof@ucdavis.edu>
2015-08-17 19:35:16 -0700

    updated tie1 Mapping plots

--- plot_tie1M82G_clean.R
+++ plot_tie1M82G_clean.R
@@ -4,19 +4,17 @@ library(ggplot2)
 library(caTools) #Could also try using IRanges or zoo packages
 library(Biostrings)
 
-setwd("~/Documents/Lab Notebook support/2013/tie1_phyE/plusM82G/")
+setwd("~/git/Lavelle-tie1--2015/")
 
-data <- read.delim("PE1PE2_paired_M82G.realigned.varscan",sep="\t",as.is=T)
-
-#original: 34932
-#realigned: 34482
+data <- read.delim("PE1PE2_paired_M82G.realigned.all.varscan.gz",sep="\t",as.is=T)
 
 head(data)
+
+names(data)
 
 data <- data[,c(1:4,11)]
 
 split <- matrix(unlist(strsplit(data$Cons.Cov.Reads1.Reads2.Freq.P.value.1,split=" ")),ncol=2,byrow=T)
-
 
 colnames(split) <- c("tie1F2","M82")
 
@@ -59,48 +57,73 @@ summary(data)
 #filter data to reduce to relevant SNPs
 #filters
 #M82 coverage
-data.small <- data[data$M82.cov > 4 & data$M82.cov < 200 ,]        
+qplot(data$M82.cov,geom="histogram") + scale_x_log10()
+qplot(data$M82.cov,geom="histogram") + xlim(0,100)
+qplot(data$M82.cov,geom="histogram") + xlim(0,200)
+
+data.small <- data[data$M82.cov > 9 & data$M82.cov < 200 ,] 
+qplot(data.small$M82.cov,geom="histogram") + xlim(0,200)
+
 
 data.small <- data.small[grepl("[GATC]",data.small$M82.cons),] #get rid of M82 hets
-data.small <- data.small[data.small$M82.percent2>75,] #focus on SNPs where M82 is different from Heinz
+
+qplot(data.small$M82.percent2,geom="histogram")
+qplot(data.small$M82.percent2,geom="histogram",binwidth=1) + xlim(80,101)
+
+
+data.small <- data.small[data.small$M82.percent2>98,] #focus on SNPs where we are confident that M82 is different from Heinz
 
 #tie1 coverage
-data.small <- data.small[data.small$tie1.cov > 4 & data.small$tie1.cov < 100 ,]
 
-data.small <- data.small[!is.na(data.small$tie1.ref),] #17883 old; #12321 realigned; keeping M82 hets means 24,109
+qplot(data.small$tie1.cov,geom="histogram") + scale_x_log10()
+qplot(data.small$tie1.cov,geom="histogram",binwidth=1) + xlim(0,25)
+qplot(data.small$tie1.cov,geom="histogram") + xlim(0,100)
+
+
+data.small <- data.small[data.small$tie1.cov > 4 & data.small$tie1.cov < 50 ,]
+
+data.small <- data.small[!is.na(data.small$tie1.ref),] 
+data.small <- data.small[!is.na(data.small$M82.ref),] 
 
 #instead of using a smoothed line, compute running average.  The reason is that 
 #smoothed lines are overly infleunced by the SNP density
 
 data.small$tie1.percent.run <- caTools::runmean(data.small$tie1.percent,20)
-data.small$tie1.het.Pvalue.run <- caTools::runmean(data.small$tie1.het.Pvalue,5)
+#data.small$tie1.het.Pvalue.run <- caTools::runmean(data.small$tie1.het.Pvalue,5)
 
+data.small <- data.small[data.small$Chrom!="SL2.40ch00",]
 ##plot some of this 
 
-plotSnp <- function(start=NULL,finish=NULL,data=data.small,title=NULL,alpha=0.5) {
-  pl <- ggplot(data=data,mapping=aes(x=Position,y=tie1.percent))
+plotSnp <- function(start=NULL,finish=NULL,data=data.small,title=NULL,alpha=0.5,chrom=NULL) {
+  if(! is.null(chrom)) {
+    data <- data[data$Chrom==chrom,]
+  }
+  pl <- ggplot(data=data,mapping=aes(x=Position))
+  pl <- pl + geom_point(aes(y=tie1.percent2),alpha=alpha)
   if(!is.null(start) & ! is.null(finish)) {
     pl <- pl + xlim(c(start,finish))
-    pl <- pl + scale_size_discrete(range=c(2,6))
+    #pl <- pl + scale_size_discrete(range=c(2,6))
   }
-  pl <- pl + geom_point(alpha=alpha)
-  pl <- pl + geom_line(aes(y=tie1.percent.run),color="blue",lwd=1)
- # pl <- pl + geom_line(aes(y=-log10(tie1.het.Pvalue.run)))
   if(!is.null(title)) {
     pl <- pl + ggtitle(title)
   }
+  if(is.null(chrom)) {
+    pl <- pl + facet_wrap(~ Chrom, ncol = 2)
+    #pl <- pl + geom_smooth(aes(y=tie1.percent2),color="skyblue",lwd=1) + ylim(0,100)
+}# else {
+  pl <- pl + geom_line(aes(y=tie1.percent.run),color="skyblue",lwd=1)
+#}
   print(pl)
 }
+  
+plotSnp(title="tie1 F2 BSA Analysis")
+ggsave("tie1_F2_BSA_All_Chroms.pdf",height=11,width=8.5)
 
-plotSnp(title="Chrom 2") #essentially two regions, around 3.4- 3.6 and 3.9
+plotSnp(title="tie1 F2 BSA Analysis Chrom 2",chrom="SL2.40ch02") #essentially two regions, around 3.4- 3.6 and 3.9
+ggsave("tie1_F2_BSA_Ch02.pdf",width=4,height=4)
 
-plotSnp(3e07,4e07,title="Chrom 2 Enlargement")
-
-plotSnp(2.8e07,4.2e07,title="Chrom 2 Enlargement")
-
-plotSnp(3.3e07,3.51e07,title="Chrom 2 Enlargement")
-
-plotSnp(3.9e07,4.1e07,title="Chrom 2 Enlargement")
+plotSnp(2.8e07,4.2e07,title="Chrom 2 Enlargement",chrom="SL2.40ch02")
+ggsave("tie1_F2_BSA_Ch02.pdf",width=4,height=4)
 
 #get nearby genes
 
