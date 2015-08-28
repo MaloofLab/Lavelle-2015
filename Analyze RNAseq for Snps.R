88a4bb2ed4eff70828257d9486b724a10ab373b5
Julin Maloof <jnmaloof@ucdavis.edu>
2015-08-27 17:59:27 -0700

    working on SNP effect prediction

--- Analyze RNAseq for Snps.R
+++ Analyze RNAseq for Snps.R
@@ -1,29 +1,38 @@
 #Looking for interesting SNPs in RNAseq data
-#Julin MAloof
+#Julin Maloof
 #December 2, 2013
 
-setwd("~/Documents/Lab Notebook support/2013/tie1_phyE/RNAseq/")
-data <- read.delim("anotated_M82_tie1.varscan.vcf",comment.char="#",sep="\t",header=F,as.is=T)
+data <- read.delim("anotated_M82_tie1.varscanNEW.vcf",comment.char="#",sep="\t",header=F,as.is=T)
 head(data)
 colnames(data) <- unlist(strsplit("CHROM  POS     ID      REF     ALT     QUAL    FILTER  INFO    FORMAT  M82 tie1",split=" +"))
 head(data,20)
 
 data$tie1 <- sub("(\\./\\.:\\.:[0-9]+$)","\\1::::::::::::",data$tie1)
 
-tie1 <- matrix(unlist(strsplit(data[,"tie1"],split=":")),ncol=14,byrow=T)
+tie1 <- as.data.frame(matrix(unlist(strsplit(data[,"tie1"],split=":")),ncol=14,byrow=T))
 head(tie1)
 colnames(tie1) <- paste("tie1",c("genotype","genotypeQuality","raw","cov",
                                  "ref","alt","percent","pval","r.qual","alt.qual",
                                  "ref.for","ref.rev","alt.for","alt.rev"),sep=".")
+head(tie1)
+
+tie1$tie1.percent <- sub("%","",tie1$tie1.percent)
+
+tie1[,-1] <- apply(tie1[,-1],2,as.numeric)
 
 data$M82 <- sub("(\\./\\.:\\.:[0-9]+$)","\\1::::::::::::",data$M82)
 
-M82 <- matrix(unlist(strsplit(data[,"M82"],split=":")),ncol=14,byrow=T)
+M82 <- as.data.frame(matrix(unlist(strsplit(data[,"M82"],split=":")),ncol=14,byrow=T))
 head(M82)
 colnames(M82) <- paste("M82",c("genotype","genotypeQuality","raw","cov",
                                  "ref","alt","percent","pval","r.qual","alt.qual",
                                  "ref.for","ref.rev","alt.for","alt.rev"),sep=".")
 head(M82)
+
+M82$M82.percent <- sub("%","",M82$M82.percent)
+
+M82[,-1] <- apply(M82[,-1],2,as.numeric)
+
 summary(M82)
 
 data.large <- cbind(data,tie1[,1:8],M82[,1:8])
@@ -33,12 +42,13 @@ head(data.small,30)
 summary(data.small)
 data.small <- data.small[as.character(data.small$M82.genotype) != as.character(data.small$tie1.genotype),]
 head(data.small,30)
-data.small <- data.small[data.small$tie1.genotype!="0/1",]
+data.small <- data.small[data.small$tie1.genotype=="1/1",]
+data.small <- data.small[data.small$tie1.percent>90,]
 
 data.small <- data.small[grep("EFF",data.small$INFO),]
 data.small <- data.small[!grepl("=SYNONYMOUS_CODING",data.small$INFO),]
 
-data.small <- data.small[data.small$M82.genotype != "0/1" | as.numeric(as.character(data.small$M82.pval))>.01,]
+data.small <- data.small[data.small$M82.percent < 20 | data.small$M82.percent > 80,] # limit to nonsegregating M82 sites
 
 head(data.small)
 
